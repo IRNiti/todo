747b30fc736c8854ada126036b33fbd2a17c98bc
Raluca Niti <raluca.niti@gmail.com>
2021-11-02 20:24:19 -0700

    Adding chore model

---
+++ db/migrate/20211103032246_create_chores.rb
@@ -0,0 +1,10 @@
+class CreateChores < ActiveRecord::Migration[6.1]
+  def change
+    create_table :chores do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
