40ba6410bf4630f16e64eb39cfdc94c2a20fce1f
Raluca Niti <raluca.niti@gmail.com>
2021-11-11 17:21:39 -0800

    migrating db from sqlite3 to postgres

--- db/migrate/20211103032246_create_chores.rb
+++ db/migrate/20211103032246_create_chores.rb
@@ -1,7 +1,7 @@
 class CreateChores < ActiveRecord::Migration[6.1]
   def change
     create_table :chores do |t|
-      t.string :name
+      t.text :name
       t.text :description
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_11_05_014628) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "chores", force: :cascade do |t|
     t.string "name"
     t.text "description"
