ab53c7f4af2a0653e8332b968580d5fcce7c1e64
Raluca Niti <raluca.niti@gmail.com>
2021-12-08 21:36:13 -0800

    Adding code coverage for restricted profile access

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  describe "GET users" do 
+    it "displays the user profile for a logged in user" do 
+      user = create(:user)
+      post login_path, params: { session: { email: "betty.rubble@gmail.com", password: "foobarzz" } }
+      get user_path(user)
+      expect(response.body).to include("betty.rubble@gmail.com")
+    end
+
+    it "does not display another user's profile even for a logged in user" do 
+      user1 = create(:user)
+      user2 = build(:user)
+      user2.email = "test@test.com"
+      user2.save
+      post login_path, params: { session: { email: "betty.rubble@gmail.com", password: "foobarzz" } }
+      get user_path(user2)
+      expect(response.body).to include("You do not have access to this information.")
+    end
+
+    it "redirects to the login page when a non logged in user tries to view a user profile" do 
+      user = create(:user)
+      get user_path(user)
+      assert_redirected_to login_path
+    end
+  end
+  
   describe "GET /new" do
     it "returns http success" do
       get "/users/new"
