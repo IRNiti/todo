f8305c9e11c1f541538062edbff56bd1b670401a
Raluca Niti <raluca.niti@gmail.com>
2021-12-15 18:26:17 -0800

    Make assigned user optional on chores

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,5 +1,5 @@
 class Chore < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 10 }
 end
