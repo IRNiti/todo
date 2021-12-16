5525657fe7c6c5a07c748da8cbd060b82c18980a
Raluca Niti <raluca.niti@gmail.com>
2021-12-15 17:57:22 -0800

    Updating UI to show chores assigned to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @chores_done = Chore.where(done: true, user_id: params[:id])
+    @chores_in_progress = Chore.where(done: false, user_id: params[:id])
   end
 
   def new
